8219652dca1140e39f5fba7956ab33b0c730e9a8
Célestin Rumo <celestin.rumo@asyril.com>
2022-09-09 16:19:20 +0200

    logger/first commit of minimal viable version
    without router or anything. just single page

---
+++ src/components/LogDisplay.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Title from './Title';
+
+// Generate Order Data
+
+
+export default function Orders()
+{
+  return (
+    <React.Fragment>
+      <Title>Recent Orders</Title>
+    </React.Fragment>
+  );
+}
--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 function Title(props)
 {
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography component="h2" variant="h6" color="black" gutterBottom>
       {props.children}
     </Typography>
   );
