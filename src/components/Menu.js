dbf566f63a611a8a09c0b17ad4e43b05dace22f3
Valentin Longchamp <valentin.longchamp@asyril.com>
2022-09-09 17:01:31 +0200

    use a MenuItem function to factor out the LIstItemButton

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,21 +1,20 @@
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
-import * as React from 'react';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+function MenuItem(props) {
+  return (
+    <ListItemButton>
+      <ListItemText primary={props.value} />
+    </ListItemButton>
+  );
+}
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton>
-      <ListItemText primary="Eye Plus" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemText primary="Asycube Studio" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemText primary="Merlin" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemText primary="Hopper" />
-    </ListItemButton>
+    <MenuItem value="Eye Plus" />
+    <MenuItem value="Asycube Studio" />
+    <MenuItem value="Merlin" />
+    <MenuItem value="Hopper" />
   </React.Fragment>
 );
