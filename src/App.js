953b4bcc3183ae00705f45b9b9db80c2ebdabdbb
Célestin Rumo <celestin.rumo@asyril.com>
2022-09-14 09:13:33 +0000

    log-viewer:base-layout:add theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AppPage from "components/layout/AppPage";
 
 function App() {
+  const mdTheme = createTheme({
+    palette: {
+      primary: {
+        light: "#52717f",
+        main: "#264653",
+        dark: "#001f2a",
+        contrastText: "#ffffff",
+      },
+      secondary: {
+        light: "#ffa07d",
+        main: "#e86f50",
+        dark: "#b14026",
+        contrastText: "#000000",
+      },
+    },
+  });
+
   return (
     <div className="App">
-      <AppPage />
-      <div className="content"></div>
+      <ThemeProvider theme={mdTheme}>
+        <AppPage />
+      </ThemeProvider>
     </div>
   );
 }
