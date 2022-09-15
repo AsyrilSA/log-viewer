def07f1070fafc95250e9b08ba061f7008985f89
Cyril Jaquier <cyril.jaquier@asyril.com>
2022-09-15 14:36:07 +0200

    parsing: implement initial parsing of log lines

--- src/utils/logParser.js
+++ src/utils/logParser.js
@@ -1,15 +1,37 @@
-function parseLogFile(logFile) {
-  // Parse you log file here in order to have an array with the followign elements
-  debugger;
-  return [
-    {
-      id: 1, // id is needed for the table we set here the line number
-      type: "Error",
-      timestamp: "2022-09-15 10:02:55",
-      service: "vision",
-      message: "message",
-    },
-  ];
+const RE_PYTHON = new RegExp(
+  /^(?<date>\S+) (?<time>\S+) (?<service>\S+) \[(?<level>\w+\s*)\] (?<message>.*)/
+);
+
+function parseLine(index, line) {
+  const match = line.match(RE_PYTHON);
+  if (match) {
+    const { date, time, service, level, message } = match.groups;
+    return {
+      id: index,
+      type: level,
+      timestamp: date + " " + time,
+      service: service,
+      message: message,
+    };
+  } else {
+    console.log("Match failed for: " + line);
+  }
+  return null;
+}
+
+function parseLogFile(logLines) {
+  var entries = [];
+
+  // By lines
+  const lines = logLines.split("\n");
+  for (var line = 0; line < lines.length; line++) {
+    const item = parseLine(line, lines[line]);
+    if (item) {
+      entries.push(item);
+    }
+  }
+
+  return entries;
 }
 
 function getCommunicationLog(logObject) {
