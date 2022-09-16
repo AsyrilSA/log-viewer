8a2b3beae4d579e9e0622bd4a58afc7329c60c7e
Valentin Longchamp <valentin.longchamp@asyril.com>
2022-09-16 15:33:00 +0200

    add Communication Logs

--- src/pages/CommunicationPage.vue
+++ src/pages/CommunicationPage.vue
@@ -1,3 +1,10 @@
 <template>
   <h4>Communication Logs</h4>
+  <LogTable :rows="logStore.getCommunicationLog"></LogTable>
 </template>
+<script lang="ts" setup>
+import { useLogStore } from 'stores/logStore';
+import LogTable from 'src/components/LogTable.vue';
+
+const logStore = useLogStore();
+</script>
