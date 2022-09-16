a9d7d73fe460b4668b03c9568b0ea8535e28c8fb
Patrick Clerc <patrick.clerc@asyril.com>
2022-09-16 12:36:20 +0200

    ignore docker-compose perso file

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,29 +11,22 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Log Viewer 
-        </q-toolbar-title>
+        <q-toolbar-title> Log Viewer </q-toolbar-title>
+
+        <q-btn
+          icon="upload_file"
+          label="Import log"
+          aria-label="Import file"
+          @click="importFile"
+        />
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Menu
-        </q-item-label>
+        <q-item-label header> Menu </q-item-label>
 
-        <MenuLink
-          v-for="link in menuLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <MenuLink v-for="link in menuLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -52,49 +45,53 @@ const menuLinks: MenuLinkProps[] = [
     title: 'Base',
     caption: 'Basic information',
     icon: 'content_paste_search',
-    link: '/#/base'
+    link: '/#/base',
   },
   {
     title: 'Github',
     caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: 'https://github.com/quasarframework',
   },
   {
     title: 'Discord Chat Channel',
     caption: 'chat.quasar.dev',
     icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    link: 'https://chat.quasar.dev',
   },
   {
     title: 'Forum',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    link: 'https://forum.quasar.dev',
   },
   {
     title: 'Twitter',
     caption: '@quasarframework',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: 'https://twitter.quasar.dev',
   },
   {
     title: 'Facebook',
     caption: '@QuasarFramework',
     icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    link: 'https://facebook.quasar.dev',
   },
   {
     title: 'Quasar Awesome',
     caption: 'Community Quasar projects',
     icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+    link: 'https://awesome.quasar.dev',
+  },
 ];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
 function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+function importFile() {
+  // TODO (clp)
 }
 </script>
