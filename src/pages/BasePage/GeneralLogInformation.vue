c4d5a40c489f0b54088628f5103b87adb0b5dbbb
Célestin Rumo <celestin.rumo@asyril.com>
2022-09-16 14:07:41 +0000

    basePage structure

---
+++ src/pages/BasePage/GeneralLogInformation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="column q-pa-md">
+    <div> 1</div>
+    <div> 2</div>
+    <div> 3</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss">
+
+</style>
