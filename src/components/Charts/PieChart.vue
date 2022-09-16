90c3e0d0b0667d823dbafbf60e6a45531ad54a25
Célestin Rumo <celestin.rumo@asyril.com>
2022-09-16 16:36:26 +0000

    add vue-chartjs

---
+++ src/components/Charts/PieChart.vue
@@ -0,0 +1,67 @@
+import { defineComponent, h, PropType } from 'vue'
+
+import { Pie } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+  Plugin
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
+
+export default defineComponent({
+  name: 'PieChart',
+  components: {
+    Pie
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'pie-chart'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+  },
+  setup(props) {
+    const chartData = {
+      labels: ['Application', 'System', 'Communication'],
+      datasets: [
+        {
+          label:"test",
+          backgroundColor: ['#41B883', '#E46651', '#00D8FF'],
+          data: [42, 23, 35]
+        }
+      ]
+    }
+
+    const chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+
+    return () =>
+      h(Pie, {
+        chartData,
+        chartOptions,
+        chartId: props.chartId,
+        width: props.width,
+        height: props.height,
+        cssClasses: props.cssClasses,
+
+      })
+  }
+})
--- src/pages/BasePage/LogRepartition.vue
+++ src/pages/BasePage/LogRepartition.vue
@@ -1,9 +1,10 @@
 <template>
-  <div></div>
+  <div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
+import PieChart from 'src/components/Charts/PieChart.vue';
 </script>
 
 <style lang="scss">
